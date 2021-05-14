dabdce7df5e9edc67c7fa2daf2571831f0185ec5
hango <hangolethanh@gmail.com>
2021-05-14 21:01:32 +0300

    done 4-15

--- Blog/tests/blog_api.test.js
+++ Blog/tests/blog_api.test.js
@@ -4,6 +4,8 @@ const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -141,6 +143,39 @@ describe('deletion of a blog', () => {
     const titles = blogsAtEnd.map(blog => blog.title)
 
     expect(titles).not.toContain(blogToDelete.title)
+  })
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({username: 'root', passwordHash})
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'hango',
+      name: 'Ha Ngo',
+      password: 'hangole',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(user => user.username)
+    expect(usernames).toContain(newUser.username)
   })
 })
 
