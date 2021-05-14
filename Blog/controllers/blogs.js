e77c6a118b1102fe761967bf286c7c47e8053499
hango <hangolethanh@gmail.com>
2021-05-14 22:06:00 +0300

    done 4-17

--- Blog/controllers/blogs.js
+++ Blog/controllers/blogs.js
@@ -1,20 +1,37 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response, next) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map((blog) => blog.toJSON()))
  
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
-  if (!blog.title || !blog.url) {
-    return response.status(400).end()
-  }
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+  // const blog = new Blog(request.body)
+  // if (!blog.title || !blog.url) {
+  //   return response.status(400).end()
+  // }
+  // const savedBlog = await blog.save()
+  // response.status(201).json(savedBlog)
 
+  const body = request.body
+  
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog ({
+    title: body.title,
+    url: body.url,
+    author: body.url,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  
+  response.json(savedBlog)
 })
 
 blogsRouter.get('/:id', async (request, response) => {
