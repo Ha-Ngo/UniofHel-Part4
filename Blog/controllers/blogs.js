8b862fc6feabd46323a80d30de1174c3814508bb
hango <hangolethanh@gmail.com>
2021-05-17 11:57:02 +0300

    done 4-20

--- Blog/controllers/blogs.js
+++ Blog/controllers/blogs.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if(authorization && authorization.toLowerCase().startWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//     return authorization.substring(7)
+//   }
+//   return null
+// }
 
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -27,9 +27,10 @@ blogsRouter.post('/', async (request, response) => {
   // response.status(201).json(savedBlog)
 
   const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if(!token || !decodedToken.id) {
+  // const token = getTokenFrom(request)
+  console.log(request.token)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if(!request.token || !decodedToken.id) {
     return response.status(401).json({error: 'token missing or invalid'})
   }
   
